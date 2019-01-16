7e8753f271984ca3bd0a678c4c5868ee7c1fdfa2
Stephen George <stephenngeorge@gmail.com>
2019-01-16 19:58:36 +0000

    updated events

--- src/components/Pages/HomePageComponents/Events.js
+++ src/components/Pages/HomePageComponents/Events.js
@@ -9,12 +9,11 @@ import calendar_icon from '../../../assets/calendar_icon.png'
 export default () => {
   // array of upcoming events, mapped to array of <Event /> components in return()
   const eventsList = [
-    { type: 'GIFTING DAY', location: 'Tannery House, Bakewell', date: new Date(1500073200000) },
-    { type: 'CLERGY SPOUSE DAY', location: 'St Saviour\'s, Sunbury', date: new Date(1504911600000) },
-    { type: 'SHORT COURSE (SESSION FOUR)', location: 'Tearfund (Staff Only)', date: new Date(1505689200000) },
-    { type: 'CLERGY SPOUSE DAY', location: 'Salisbury', date: new Date(1506726000000) },
-    { type: 'GIFTING DAY', location: 'Graves End', date: new Date(1507330800000) },
-    { type: 'GIFTING DAY', location: 'St Peter & St Paul\'s Church, Nottingham', date: new Date(1509753600000) }
+    { type: 'GIFTING DAY', location: 'Newark Salvation Army, NG24 3GA', date: new Date('March 2, 2019 09:00:00') },
+    { type: 'SHORT COURSE (SESSION ONE)', location: 'Tearfund (Staff Only)', date: new Date() },
+    { type: 'WORSHIP & THE WORD SERVICE', location: 'St Giles\' Church, Ickenham', date: new Date('March 17, 2019 09:00:00') },
+    { type: 'CLERGY WIVES RETREAT', location: 'Shipston-on-Stour, Warwickshire', date: new Date('March 26, 2019 09:00:00') },
+    { type: 'GIFTING DAY', location: 'St John the Baptist, Huddersfield', date: new Date('June 8, 2019 09:00:00') }
   ]
   return (
     <section className='upcoming-events'>
