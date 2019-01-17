4c91645c397d572b4aff4dd334dc0acefe8d0cde
Stephen George <stephenngeorge@gmail.com>
2019-01-17 13:35:27 +0000

    update affiliation to ijm

--- src/components/Globals/Affiliation.js
+++ src/components/Globals/Affiliation.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 // import assets
-import restored_logo from '../../assets/restored-logo.jpg'
+import ijm_logo from '../../assets/ijm_logo.jpg'
 
 export default () => (
   <div className='affiliation'>
-    <img src={ restored_logo } alt='restored-logo' />
-    <p>Esther Ministry is proud to support<br /><a href='http://www.restoredrelationships.org/'>'Restored Relationships'</a></p>
+    <a href='https://www.ijm.org/' target="_blank" rel="noopener noreferrer">
+      <img src={ ijm_logo } alt='ijm-logo' />
+    </a>
+    <p>Esther Ministry is proud to support<br /><a href='https://www.ijm.org/' target="_blank" rel="noopener noreferrer">'International Justice Mission'</a></p>
   </div>
 )
