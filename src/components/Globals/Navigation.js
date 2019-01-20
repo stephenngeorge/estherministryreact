a018ec565a0eb059cf5e896d1e0d48133ed1e754
Stephen George <stephenngeorge@gmail.com>
2019-01-20 14:56:55 +0000

    responsive navbar

--- src/components/Globals/Navigation.js
+++ src/components/Globals/Navigation.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 // import assets
 import Esther_Logo_Green from '../../assets/Esther_Logo_Green.jpg'
 
-const Navigation = () => {
-// UNCOMMENT THIS CODE IF LOGIN PAGE IS REQUIRED
-//  const _logout = () => {
-//    sessionStorage.removeItem('auth');
-//  }
 
-//  let logInOut = !!sessionStorage.getItem('auth') ? <a href='/' onClick={ _logout } id='logout-btn'>LOGOUT</a> : <Link to='/login'><img id='padlock-icon' src='../img/padlock_icon.png' /></Link>;
-//  let resources = !!sessionStorage.getItem('auth') ? <li><Link to='/resources'>RESOURCES</Link></li> : null;
-  return (
-    <div className='navigation'>
-      <Link to='/' className='navbar-home'><img id='navbar-logo' src={ Esther_Logo_Green } alt='navbar logo' /><p>ESTHER MINISTRY</p></Link>
-      <ul className='navbar'>
-        <li><Link to='/about'>ABOUT</Link></li>
-        <li><Link to='/advisory-board'>ADVISORY BOARD</Link></li>
-        <li><Link to='/contact'>CONTACT</Link></li>     
-      </ul>
-    </div>
-  );
+class Navigation extends Component {
+
+  toggleActive = () => {
+    const navbar = document.querySelector('.navbar')
+    const icon = document.querySelector('.icon')
+    navbar.classList.toggle('active')
+    icon.classList.toggle('icon-active')
+  }
+
+  render() {
+    return (
+      <div className='navigation'>
+        <Link to='/' className='navbar-home'><img id='navbar-logo' src={ Esther_Logo_Green } alt='navbar logo' /><p>ESTHER MINISTRY</p></Link>
+        <ul className='navbar'>
+          <li><Link to='/about'>ABOUT</Link></li>
+          <li><Link to='/advisory-board'>ADVISORY BOARD</Link></li>
+          <li><Link to='/contact'>CONTACT</Link></li>     
+        </ul>
+        <div className='icon' onClick={ this.toggleActive }><div className='menu-icon'></div></div>
+      </div>
+    )
+  }
 }
 
 
@@ -32,3 +37,11 @@ export default Navigation
 
 // render this below ul if login stuff is required, & uncomment line 13
 // { logInOut }
+
+// UNCOMMENT THIS CODE IF LOGIN PAGE IS REQUIRED
+//  const _logout = () => {
+//    sessionStorage.removeItem('auth');
+//  }
+
+//  let logInOut = !!sessionStorage.getItem('auth') ? <a href='/' onClick={ _logout } id='logout-btn'>LOGOUT</a> : <Link to='/login'><img id='padlock-icon' src='../img/padlock_icon.png' /></Link>;
+//  let resources = !!sessionStorage.getItem('auth') ? <li><Link to='/resources'>RESOURCES</Link></li> : null;
