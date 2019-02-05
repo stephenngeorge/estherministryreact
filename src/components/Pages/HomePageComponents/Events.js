16e5045e097ad3d9e8e29f37288cbfc0ce963c85
Stephen George <stephenngeorge@gmail.com>
2019-02-05 16:21:13 +0000

    latest build

--- src/components/Pages/HomePageComponents/Events.js
+++ src/components/Pages/HomePageComponents/Events.js
@@ -10,10 +10,15 @@ export default () => {
   // array of upcoming events, mapped to array of <Event /> components in return()
   const eventsList = [
     { type: 'GIFTING DAY', location: 'Newark Salvation Army, NG24 3GA', date: new Date('March 2, 2019 09:00:00') },
-    { type: 'SHORT COURSE (SESSION ONE)', location: 'Tearfund (Staff Only)', date: new Date() },
     { type: 'WORSHIP & THE WORD SERVICE', location: 'St Giles\' Church, Ickenham', date: new Date('March 17, 2019 09:00:00') },
     { type: 'CLERGY WIVES RETREAT', location: 'Shipston-on-Stour, Warwickshire', date: new Date('March 26, 2019 09:00:00') },
-    { type: 'GIFTING DAY', location: 'St John the Baptist, Huddersfield', date: new Date('June 8, 2019 09:00:00') }
+    { type: 'SPEAKING', location: 'St Edyth\'s, Bristol', date: new Date('May 12, 2019 18:30:00') },
+    { type: 'SHORT COURSE (SESSION ONE)', location: 'Tearfund (Staff Only)', date: new Date('May 13, 2019 09:00:00') },
+    { type: 'SHORT COURSE (SESSION TWO)', location: 'Tearfund (Staff Only)', date: new Date('June 3, 2019 09:00:00') },
+    { type: 'GIFTING DAY', location: 'St John the Baptist, Huddersfield', date: new Date('June 8, 2019 09:00:00') },
+    { type: 'SHORT COURSE (SESSION THREE)', location: 'Tearfund (Staff Only)', date: new Date('July 1, 2019 09:00:00') },
+    { type: 'SHORT COURSE (SESSION FOUR)', location: 'Tearfund (Staff Only)', date: new Date('9 September, 2019 09:00:00') },
+    { type: 'GIFTING DAY', location: 'Portsmouth', date: new Date('September 29, 2019 09:00:00') }
   ]
   return (
     <section className='upcoming-events' role='complementary'>
